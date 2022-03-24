0b2c6c4078b0d9783f439aa504468733d819fe2f
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-03-24 17:53:42 +0900

    Add files via upload

---
+++ Python/Module2.py
@@ -0,0 +1,5 @@
+def func1():
+    print('Module2 --> func1()')
+    
+#test    
+func1()
---
+++ Python/mypackage/Module3.py
@@ -0,0 +1,9 @@
+def func1():
+    print('Module3 --> func1()')
+
+    
+    
+    
+if __name__=='__main__':
+#test    
+    func1()
