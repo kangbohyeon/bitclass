88d8c3eb803ec480a05dc36e12db5eb36567d4df
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-03-30 21:17:16 +0900

    Update programming05.java

--- JAVA/문제풀이/chap02/programming05.java
+++ JAVA/문제풀이/chap02/programming05.java
@@ -1,9 +1,12 @@
 package chap02_practice;
 
+import java.util.Scanner;
+
 public class programming05 {
 
 	public static void main(String[] args) {
-		char ch = 'z';
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.next().charAt(0);
 		int n = (int) 'A' - (int) 'a';
 		System.out.printf("%c", ch + n);
 	}
