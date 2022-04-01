bc9be245e85d7a2429774d4d3afbe4aaa7540571
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-04-01 14:08:53 +0900

    Add files via upload

--- JAVA/문제풀이/chap03/programming08.java
+++ JAVA/문제풀이/chap03/programming08.java
@@ -1,17 +1,18 @@
 package chap03_practice;
 
 public class programming08 {
-
 	public static void main(String[] args) {
+
 		System.out.println(factorial(5));
 
 	}
 
-	private static int factorial(int n) {
-		int i, result = 1;
-		return switch(n) {
-		case n -> "1°³";
-	}
+	static int factorial(int n) {
+
+		return switch (n) {
+		case 1 -> 1;
+		default -> n * factorial(n - 1);
+		};
 	}
 
 }
