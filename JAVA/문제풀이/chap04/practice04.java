8565cca64008e116375c3ad1947b6b9600569306
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-04-01 09:55:06 +0900

    Add files via upload

---
+++ JAVA/문제풀이/chap04/practice04.java
@@ -0,0 +1,51 @@
+package chap04_parctice;
+
+/* 1. 2 - 클래스를 정의하면 기본생성자가 정의되어야합니다.
+ * 2. 2
+ * 3. x
+ * 4. o
+ * 5. o
+ * 6. 다형성
+ * 
+ */
+
+public class practice04 {
+	public static void main(String[] args) {
+		// 7
+		/*
+		 * class utill{ public double findPI() { return 3.14; } } public class prac {
+		 * public static void main(String[] args) { utill u = null;
+		 * System.out.println(u.findPI()); } }
+		 * 
+		 * 
+		 * 
+		 * 8 s.i=3, o.i=1 class sklass{ static int i; sklass(){ i++; } } class oklass{
+		 * int i; oklass(){ i++; } }
+		 * 
+		 * public class prac { public static void main(String[] args) { new sklass();new
+		 * sklass(); sklass s= new sklass(); new oklass();new oklass();oklass o =new
+		 * oklass(); System.out.printf("s.i = %d o.i= %d",s.i,o.i);
+		 * 
+		 * }
+		 * 
+		 * }
+		 * 
+		 * 
+		 * 9. this가 먼저 호출해야함 public class Student{ String name;
+		 * 
+		 * Student(String name){ this.name=name; }
+		 * 
+		 * Student(){ this("no name"); System.out.println("no argument");
+		 * 
+		 * 
+		 * 
+		 * 10. public class FooTest { public static void main(String[] args) { show();
+		 * 
+		 * } public static void show() { System.out.println("show"); }
+		 * 
+		 * }
+		 * 
+		 */
+	}
+
+}
