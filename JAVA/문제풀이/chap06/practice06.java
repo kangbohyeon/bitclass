2b4b32360a172eee5c538474913e3ec7cb5eb5b8
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-04-05 23:59:43 +0900

    Add files via upload

---
+++ JAVA/문제풀이/chap06/practice06.java
@@ -0,0 +1,24 @@
+package java_2022_4_5;
+
+/* chap06연습문제
+ * #1 - 2
+ * #2 - 3
+ * #3 - 오류(상속받지않아 비교할수 없음)
+ * #4 - 1
+ * #5 - 부모생성자를 받지 않아 오
+ * #6 - void number(){} [접근지정자가 없어 오류] , static void weight() {} [정적으로는 상속할수없다]
+ * 	  - public void name() {}
+ * 		static void show() {}
+		private void secret() {}
+ *  #7 - 1 
+ * 
+ */
+
+
+public class practice06 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
