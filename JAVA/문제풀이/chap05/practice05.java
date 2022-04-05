8a9c4378c101a8f9b564440632a03d0cd6469580
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-04-05 23:59:20 +0900

    Add files via upload

---
+++ JAVA/문제풀이/chap05/practice05.java
@@ -0,0 +1,31 @@
+package java_2022_4_5;
+/* chap5장 연습문제
+ * #1 - 2
+ * #2 - 4
+ * #3 - 4
+ * #4 - 2 
+ * #5 - false
+ * #6 - 프로그래밍
+ * #7 - 213
+ * #8 - double [] a = {1.0,2.0,3.0};
+ * #9 - java false1
+ * #10 - 
+ * enum Size{
+	SMALL("S"),MEDIUM("M"),LARGE("L");
+	String abb;
+
+	private Size(String abb) {
+		this.abb = abb;
+}
+
+}
+ * #11 - for (int i = 0; i < n.length; i++)
+ * 		printArray(int[] {1,2,3,4});
+ */
+
+public class practice05 {
+
+	public static void main(String[] args) {
+
+	}
+}
