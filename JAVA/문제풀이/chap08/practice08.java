5aff0989e439f72b15589303c4446d2ef8f929f5
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-04-07 00:28:26 +0900

    Add files via upload

---
+++ JAVA/문제풀이/chap08/practice08.java
@@ -0,0 +1,39 @@
+package java_2022_4_5;
+import java.text.DecimalFormat;
+import java.util.Random;
+
+/*
+ * #1 - 3
+ * #2 - 4
+ * #3 - 4 parseInteger -> parseInt
+ * #4 - 3
+ * #5 - 2
+ * #6 - x
+ * #7 - o
+ * #8 - false
+ * 				System.out.println(d1.compareTo(d2));
+ * #9 - 123457
+ * #10 int x;
+		Random random = new Random();
+		for(int i = 0; i<10;i++) {
+			x= (int) ((Math.random()* 6));
+			System.out.print(x+" ");
+		}
+
+ */
+
+
+
+public class practice08 {
+
+	public static void main(String[] args) {
+		int x;
+		Random random = new Random();
+		for(int i = 0; i<10;i++) {
+			x= (int) ((Math.random()* 6));
+			System.out.print(x+" ");
+		}
+		
+	}
+
+}
