b64983ae1961c277c5a8de087248dbf9469c6e07
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-04-07 00:27:31 +0900

    Add files via upload

---
+++ JAVA/문제풀이/chap07/practice07.java
@@ -0,0 +1,29 @@
+package java_2022_4_5;
+
+/*
+ * #1 - 3 상수가능
+ * #2 - 2
+ * #3 - 2
+ * #4 - 4
+ * #5 - o
+ * #6 - o
+ * #7 - x
+ * #8 - implements
+ * #9 - 6행 extends -> Implements ,8행 삭제 
+ * #10 - WhatIsThis.this.value
+ * #11 - InnerDemo.class.class , InnerDemo$1LocalClass.class, 
+ * InnerDemo$2LocalClass.class, InnerDemo$MemberClass.class, InnerDemo$MemberInterface.class
+ */
+
+
+
+
+public class practice07 {
+
+	
+	public static void main(String[] args) {
+	
+		
+	}
+	
+}
