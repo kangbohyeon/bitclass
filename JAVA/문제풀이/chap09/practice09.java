0d702f760960378327d24346d444f37cb200c946
kangbohyeon <100145105+kangbohyeon@users.noreply.github.com>
2022-04-08 01:12:53 +0900

    Add files via upload

---
+++ JAVA/문제풀이/chap09/practice09.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class practice09 {
+/*
+ * #1 - 2 (비검사형 예외)
+ * #2 - catch(Exception e){} 와 catch(NumberFormatException e){}자리교체
+ *      Exception객체를 처리하는 catch 블록에서 모든 예외를 처리하므로 
+ *      catch(NumberFormatException e){} 이 블록에 도달할수 없으므로
+ * #3 - x 
+ * #4 - T[]
+ * 		temp = array[a];
+ * #5 - o
+ * #6 - 4
+ * #7 - 클래스를 찾을 수 없다.
+ * 		어이쿠!
+ * #8 - 안녕!
+ * 		언제나 희망이 있다.
+ * #9  - ArrayList<Integer> list = new ArrayList<>();변환하면 에러 삭제됨
+ * #10 - <T> T
+ * 		 T[]a
+ * 
+ * 
+ * 
+ * 
+ */
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+//	#10
+//	public static <T> T getLast(T[]a){
+//		return a[a.length -1];
+//	}
+
+}
