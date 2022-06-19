58a6f21aad0fcb9a69bafe956b06ed191c3dfb2b
kang bo hyun <100145105+kangbohyeon@users.noreply.github.com>
2022-06-19 15:38:58 +0900

    Add files via upload

---
+++ Back_end/test/src/test/java/io/namoosori/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.namoosori.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
